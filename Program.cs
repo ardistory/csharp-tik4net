8db812217313d9a7a148b475b9e8aff133d7bd2b
Edp Network G157 <edpnetlbk@outlook.com>
2024-02-22 19:57:21 +0700

    CSharp Project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HumanIsLife
+
+
+{
+    class Program
+    {
+        string? firstName;
+        string? lastName;
+
+        Program(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public void getFullName()
+        {
+            Console.WriteLine(this.firstName +" "+this.lastName);
+        }
+
+        static void Main(string[] args)
+        {
+            Program myObj = new Program("Ardi", "Putra");
+            myObj.getFullName();
+        }
+    }
+}
