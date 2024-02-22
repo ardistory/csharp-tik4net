ecaddb67bbbed0baba8e0035eea48a548b5a301f
Edp Network G157 <edpnetlbk@outlook.com>
2024-02-22 23:26:24 +0700

    Dependency tik4net

--- Program.cs
+++ Program.cs
@@ -4,24 +4,10 @@
 {
     class Program
     {
-        string? firstName;
-        string? lastName;
-
-        Program(string firstName, string lastName)
-        {
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
-
-        public void getFullName()
-        {
-            Console.WriteLine(this.firstName +" "+this.lastName);
-        }
-
         static void Main(string[] args)
         {
-            Program myObj = new Program("Ardi", "Putra");
-            myObj.getFullName();
+            var Api = new RouterosAPI();
+            Api.setConnection();
         }
     }
 }
---
+++ RouterosAPI.cs
@@ -0,0 +1,20 @@
+﻿using tik4net;
+using tik4net.Api;
+
+namespace HumanIsLife
+{
+    class RouterosAPI
+    {
+        public void setConnection()
+        {
+            var connection = tik4net.ConnectionFactory.OpenConnection(tik4net.TikConnectionType.Api,"10.73.20.1",8278,"prom1se","asyncawait321");
+            var data = connection.CreateCommand("/ip/address/print").ExecuteList();
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
